d3eff1b47417118b8b229923bf84c119ce00bed5
Erick <ekmoya@proton.me>
2023-11-04 21:49:52 +0000

    [NH] :hammer: Update all methods using files logic as query methods were already done.

--- src/main/java/servicios/ServicioInsumo.java
+++ src/main/java/servicios/ServicioInsumo.java
@@ -17,9 +17,14 @@ import javax.jws.WebParam;
 import java.io.FileReader;
 import java.io.FileWriter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Optional;
 
 /**
@@ -29,22 +34,34 @@ import java.util.Optional;
 @WebService(serviceName = "ServicioInsumo")
 public class ServicioInsumo {
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
-    }
-
     @WebMethod(action = "agregarInsumo")
-    public String agregarInsumo(int codigo, String nombre, int valor) {
+    public String agregarInsumo(
+            @WebParam(name = "codigo") int codigo,
+            @WebParam(name = "nombre") String nombre,
+            @WebParam(name = "valor") int valor) {
         String salida;
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         Response r = new Response();
 
-        r.setStatus(200);
-        r.setData("Agregado el insumo " + nombre);
+        try {
+            JsonReader reader = new JsonReader(new FileReader("productos.json"));
+            List<Insumo> insumos = gson.fromJson(reader, new TypeToken<List<Insumo>>() {
+            }.getType());
+
+            Insumo newInsumo = new Insumo(codigo, nombre, valor);
+
+            insumos.add(newInsumo);
+
+            try (Writer writer = new FileWriter("productos.json")) {
+                gson.toJson(insumos, writer);
+            }
+
+            r.setStatus(200);
+            r.setData("Agregado el insumo " + nombre);
+        } catch (JsonIOException | JsonSyntaxException | IOException e) {
+            r.setStatus(500);
+            r.setData("Internal server error");
+        }
 
         salida = gson.toJson(r);
 
@@ -52,13 +69,42 @@ public class ServicioInsumo {
     }
 
     @WebMethod(action = "modificarInsumo")
-    public String modificarInsumo(int codigo, String nombre, int nuevoValor) {
+    public String modificarInsumo(
+            @WebParam(name = "codigo") int codigo,
+            @WebParam(name = "nombre") String newNombre,
+            @WebParam(name = "nuevoValor") int newValor) {
         String salida;
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         Response r = new Response();
 
-        r.setStatus(200);
-        r.setData("Insumo " + codigo + " has been successfully modified with nuevoValor: " + nuevoValor);
+        try {
+            JsonReader reader = new JsonReader(new FileReader("productos.json"));
+            List<Insumo> insumos = gson.fromJson(reader, new TypeToken<List<Insumo>>() {
+            }.getType());
+
+            Optional<Insumo> foundInsumo = insumos.stream()
+                    .filter(insumo -> insumo.getCodigo() == codigo)
+                    .findFirst();
+
+            if (foundInsumo.isPresent()) {
+                Insumo insumo = foundInsumo.get();
+                insumo.setNombre(newNombre);
+                insumo.setValor(newValor);
+
+                try (Writer writer = new FileWriter("productos.json")) {
+                    gson.toJson(insumos, writer);
+                }
+
+                r.setStatus(200);
+                r.setData("Insumo " + codigo + " has been successfully modified with nuevoValor: " + newValor);
+            } else {
+                r.setStatus(404);
+                r.setData("Insumo with codigo " + codigo + " not found");
+            }
+        } catch (JsonIOException | JsonSyntaxException | IOException e) {
+            r.setStatus(500);
+            r.setData("Internal server error");
+        }
 
         salida = gson.toJson(r);
 
@@ -71,9 +117,28 @@ public class ServicioInsumo {
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         Response r = new Response();
 
-        r.setStatus(200);
-        r.setData("Insumo " + codigo + " has been successfully deleted");
+        try {
+            JsonReader reader = new JsonReader(new FileReader("productos.json"));
+            List<Insumo> insumos = gson.fromJson(reader, new TypeToken<List<Insumo>>() {
+            }.getType());
 
+            boolean removed = insumos.removeIf(insumo -> insumo.getCodigo() == codigo);
+
+            if (removed) {
+                try (Writer writer = new FileWriter("productos.json")) {
+                    gson.toJson(insumos, writer);
+                }
+
+                r.setStatus(200);
+                r.setData("Insumo " + codigo + " has been successfully deleted");
+            } else {
+                r.setStatus(404);
+                r.setData("Insumo with codigo " + codigo + " not found");
+            }
+        } catch (JsonIOException | JsonSyntaxException | IOException e) {
+            r.setStatus(500);
+            r.setData("Internal server error");
+        }
         salida = gson.toJson(r);
 
         return salida;
@@ -91,7 +156,7 @@ public class ServicioInsumo {
             }.getType());
             r.setStatus(200);
             r.setData(insumos);
-        } catch (Exception e) {
+        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
             r.setStatus(500);
             r.setData("Error reading 'productos.json'");
         }
@@ -122,7 +187,7 @@ public class ServicioInsumo {
                 r.setStatus(404);
                 r.setData("Not found");
             }
-        } catch (Exception e) {
+        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
             r.setStatus(500);
             r.setData("Internal server error");
         }
